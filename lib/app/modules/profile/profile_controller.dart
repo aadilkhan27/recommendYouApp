7192acdc0c41c74e23cb203b1a289fa02e326aba
asif anjum <asif.anjum@nexxbase.com>
2022-05-19 13:30:11 +0530

    profile tab view

--- lib/app/modules/profile/profile_controller.dart
+++ lib/app/modules/profile/profile_controller.dart
@@ -1,9 +1,38 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:recommend_you/app/core/values/colors.dart';
 
-class ProfileController extends GetxController with GetSingleTickerProviderStateMixin{
+class ProfileController extends GetxController
+    with GetSingleTickerProviderStateMixin {
+  late TabController tabController;
+
+  final List<Tab> referralTabs = const <Tab>[
+    Tab(
+      child: Text(
+        'My Posts',
+      ),
+    ),
+    Tab(
+      child: Text(
+        'Saved Posts',
+      ),
+    ),
+    Tab(
+      child: Text(
+        'Liked Posts',
+      ),
+    ),
+  ];
+
   @override
   void onInit() {
-    // TODO: implement onInit
     super.onInit();
+    tabController = TabController(vsync: this, length: referralTabs.length);
+  }
+
+  @override
+  void onClose() {
+    tabController.dispose();
+    super.onClose();
   }
 }
